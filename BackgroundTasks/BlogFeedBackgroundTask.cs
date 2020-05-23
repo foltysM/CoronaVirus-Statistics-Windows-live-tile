1c743241f1e9a1e8224a535d3014467733f9bc0e
DeenDe <przemek.mazur98@gmail.com>
2020-05-23 19:46:40 +0200

    Displaying recent data on tile

--- BackgroundTasks/BlogFeedBackgroundTask.cs
+++ BackgroundTasks/BlogFeedBackgroundTask.cs
@@ -14,6 +14,7 @@ using Windows.Data.Xml.Dom;
 using Windows.UI.Notifications;
 using Windows.Web.Syndication;
 using Newtonsoft.Json;
+using Microsoft.Toolkit.Uwp.Notifications;
 
 namespace BackgroundTasks
 {
@@ -59,7 +60,7 @@ namespace BackgroundTasks
 
             // Update the live tile with the feed items.
             //UpdateTile(feed); //TODO 
-
+            update(info);
             // Inform the system that the task is finished.
             deferral.Complete();
         }
@@ -99,7 +100,92 @@ namespace BackgroundTasks
             return export;
         }
 
+        private static void update(InfoExport info)
+        {
+            string number_of_gc = info.total_cases.ToString();
+            string number_of_gd = info.total_deaths.ToString();
+            string number_of_tc = info.new_cases_today.ToString();
+            string number_of_td = info.total_deaths.ToString();
 
+            string global_c = "Global infections: " + number_of_gc;
+            string global_d = "Global deaths: " + number_of_gd;
+            string todays_c = "Today's infections: " + number_of_tc;
+            string todays_d = "Today's deaths: " + number_of_td;
+
+            // Construct the tile content            
+            TileContent content = new TileContent()
+            {
+                Visual = new TileVisual()
+                {
+                    TileMedium = new TileBinding()
+                    {
+                        Content = new TileBindingContentAdaptive()
+                        {
+                            Children =
+                            {
+                                new AdaptiveText()
+                                {
+                                    Text = global_c
+
+                                },
+
+                                new AdaptiveText()
+                                {
+                                    Text = global_d,
+                                    HintStyle = AdaptiveTextStyle.CaptionSubtle
+                                },
+
+                                new AdaptiveText()
+                                {
+                                    Text = todays_c,
+                                    HintStyle = AdaptiveTextStyle.CaptionSubtle
+                                }
+                            }
+                        }
+                    },
+
+                    TileWide = new TileBinding()
+                    {
+                        Content = new TileBindingContentAdaptive()
+                        {
+                            Children =
+                            {
+                                new AdaptiveText()
+                                {
+                                    Text = global_c,
+                                    HintStyle = AdaptiveTextStyle.CaptionSubtle
+                                },
+
+                                new AdaptiveText()
+                                {
+                                    Text = global_d,
+                                    HintStyle = AdaptiveTextStyle.CaptionSubtle
+                                },
+
+                                new AdaptiveText()
+                                {
+                                    Text = todays_c,
+                                    HintStyle = AdaptiveTextStyle.CaptionSubtle
+                                },
+
+                                new AdaptiveText()
+                                {
+                                    Text = todays_d,
+                                    HintStyle = AdaptiveTextStyle.CaptionSubtle
+                                }
+                            }
+                        }
+                    }
+                }
+            };
+           
+            var notification = new TileNotification(content.GetXml());
+
+            // And send the notification
+            TileUpdateManager.CreateTileUpdaterForApplication().Update(notification);
+            
+            ///TileUpdateManager.CreateTileUpdaterForApplication().Clear();
+        }
         private static async Task<SyndicationFeed> GetCoronaData()
         {
             SyndicationFeed feed = null;
@@ -122,32 +208,6 @@ namespace BackgroundTasks
             return feed;
         }
 
-        private static void UpdateTile(SyndicationFeed feed)
-        {
-            // Create a tile update manager for the specified syndication feed.
-            var updater = TileUpdateManager.CreateTileUpdaterForApplication();
-            updater.EnableNotificationQueue(true);
-            updater.Clear();
-
-            // Keep track of the number feed items that get tile notifications.
-            int itemCount = 0;
-
-            // Create a tile notification for each feed item.
-            foreach (var item in feed.Items)
-            {
-                XmlDocument tileXml = TileUpdateManager.GetTemplateContent(TileTemplateType.TileWide310x150Text03);
-
-                var title = item.Title;
-                string titleText = title.Text == null ? String.Empty : title.Text;
-                tileXml.GetElementsByTagName(textElementName)[0].InnerText = titleText;
-
-                // Create a new tile notification.
-                updater.Update(new TileNotification(tileXml));
-
-                // Don't create more than 5 notifications.
-                if (itemCount++ > 5) break;
-            }
-        }
 
         // Although most HTTP servers do not require User-Agent header, others will reject the request or return
         // a different response if this header is missing. Use SetRequestHeader() to add custom headers.
